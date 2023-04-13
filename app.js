70de8de6103668b7076024947bdc46cc37aba6d1
gary909 <garywhite61@hotmail.com>
2023-04-13 12:23:54 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function splitAndMerge(string, separator) {
+    return
+}
+
+console.log(splitAndMerge("My name is John"," ")); // "M y n a m e i s J o h n"
+console.log(splitAndMerge("My name is John"," ")); // "M-y n-a-m-e i-s J-o-h-n"
+console.log(splitAndMerge("Hello World!",".")); // "H.e.l.l.o W.o.r.l.d.!"
+console.log(splitAndMerge("Hello World!",",")); // "H,e,l,l,o W,o,r,l,d,!"  
